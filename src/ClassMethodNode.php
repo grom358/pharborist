00416492b453f87b21a35dda09d8ee6398bc3d9a
Cameron Zemek <cameron.zemek@previousnext.com.au>
2014-04-19 16:21:49 +1000

    Added FunctionTrait

--- src/ClassMethodNode.php
+++ src/ClassMethodNode.php
@@ -5,10 +5,7 @@ namespace Pharborist;
  * A class method.
  */
 class ClassMethodNode extends ClassStatementNode {
-  /**
-   * @var DocCommentNode
-   */
-  protected $docComment;
+  use FunctionTrait;
 
   /**
    * @var ModifiersNode
@@ -16,64 +13,9 @@ class ClassMethodNode extends ClassStatementNode {
   protected $modifiers;
 
   /**
-   * @var TokenNode
-   */
-  protected $reference;
-
-  /**
-   * @var TokenNode
-   */
-  protected $name;
-
-  /**
-   * @var ParameterListNode
-   */
-  protected $parameters;
-
-  /**
-   * @var StatementBlockNode
-   */
-  protected $body;
-
-  /**
-   * @return DocCommentNode
-   */
-  public function getDocComment() {
-    return $this->docComment;
-  }
-
-  /**
    * @return ModifiersNode
    */
   public function getModifiers() {
     return $this->modifiers;
-  }
-
-  /**
-   * @return TokenNode
-   */
-  public function getReference() {
-    return $this->reference;
-  }
-
-  /**
-   * @return TokenNode
-   */
-  public function getName() {
-    return $this->name;
-  }
-
-  /**
-   * @return ParameterNode[]
-   */
-  public function getParameters() {
-    return $this->parameters->getParameters();
-  }
-
-  /**
-   * @return StatementBlockNode
-   */
-  public function getBody() {
-    return $this->body;
   }
 }
---
+++ src/FunctionTrait.php
@@ -0,0 +1,64 @@
+<?php
+namespace Pharborist;
+
+trait FunctionTrait {
+  /**
+   * @var DocCommentNode
+   */
+  protected $docComment;
+
+  /**
+   * @var TokenNode
+   */
+  protected $reference;
+
+  /**
+   * @var TokenNode
+   */
+  protected $name;
+
+  /**
+   * @var ParameterListNode
+   */
+  protected $parameters;
+
+  /**
+   * @var StatementBlockNode
+   */
+  protected $body;
+
+  /**
+   * @return DocCommentNode
+   */
+  public function getDocComment() {
+    return $this->docComment;
+  }
+
+  /**
+   * @return TokenNode
+   */
+  public function getReference() {
+    return $this->reference;
+  }
+
+  /**
+   * @return TokenNode
+   */
+  public function getName() {
+    return $this->name;
+  }
+
+  /**
+   * @return ParameterNode[]
+   */
+  public function getParameters() {
+    return $this->parameters->getParameters();
+  }
+
+  /**
+   * @return StatementBlockNode
+   */
+  public function getBody() {
+    return $this->body;
+  }
+}

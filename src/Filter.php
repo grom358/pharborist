6e28a76f64899e2cfd6f2ec8da5f21f21ac517f4
Cameron Zemek <cameron.zemek@previousnext.com.au>
2014-08-16 09:06:52 +1000

    Added more filters

--- src/Filter.php
+++ src/Filter.php
@@ -16,6 +16,65 @@ class Filter {
   }
 
   /**
+   * Callback to filter for specific function declaration.
+   * @param string $function_name
+   * @return callable
+   */
+  public static function isFunction($function_name) {
+    return function ($node) use ($function_name) {
+      if ($node instanceof FunctionDeclarationNode) {
+        return $node->getName()->getText() === $function_name;
+      }
+      return FALSE;
+    };
+  }
+
+  /**
+   * Callback to filter for calls to a function.
+   * @param string $function_name
+   * @return callable
+   */
+  public static function isFunctionCall($function_name) {
+    return function ($node) use ($function_name) {
+      if ($node instanceof FunctionCallNode) {
+        return $node->getName()->getText() === $function_name;
+      }
+      return FALSE;
+    };
+  }
+
+  /**
+   * Callback to filter for specific class declaration.
+   * @param string $class_name
+   * @return callable
+   */
+  public static function isClass($class_name) {
+    return function ($node) use ($class_name) {
+      if ($node instanceof ClassNode) {
+        return $node->getName()->getText() === $class_name;
+      }
+      return FALSE;
+    };
+  }
+
+  /**
+   * Callback to filter for calls to a class method.
+   * @param string $class_name
+   * @param string $method_name
+   * @return callable
+   */
+  public static function isClassMethodCall($class_name, $method_name) {
+    return function ($node) use ($class_name, $method_name) {
+      if ($node instanceof ClassMethodCallNode) {
+        $class_matches = $node->getClassName()->getText() === $class_name;
+        $method_matches = $node->getMethodName()->getText() === $method_name;
+        return $class_matches && $method_matches;
+      }
+      return FALSE;
+    };
+  }
+
+  /**
    * Callback to filter comments.
    * @param bool $include_doc_comment
    * @return callable

284022ac0146a1d73699aa4f06519c30f778b985
Cameron Zemek <cameron.zemek@previousnext.com.au>
2014-08-17 13:21:57 +1000

    Added helper methods for modifying parameter

--- src/ParameterNode.php
+++ src/ParameterNode.php
@@ -33,9 +33,18 @@ class ParameterNode extends ParentNode {
       elseif ($node->getType() === '&') {
         $this->reference = $node;
       }
+      elseif ($node instanceof VariableNode) {
+        $this->name = $node;
+      }
+      elseif ($node instanceof ExpressionNode) {
+        $this->value = $node;
+      }
     }
     elseif ($node instanceof NameNode) {
       $this->typeHint = $node;
+    }
+    elseif ($node instanceof ExpressionNode) {
+      $this->value = $node;
     }
   }
 
@@ -67,6 +76,28 @@ class ParameterNode extends ParentNode {
   }
 
   /**
+   * @param string|Node $type_hint
+   * @return $this
+   */
+  public function setTypeHint($type_hint) {
+    if (is_string($type_hint)) {
+      $type = $type_hint;
+      $type_hint = new NameNode();
+      $type_hint->append(new TokenNode(T_STRING, $type));
+    }
+    if (isset($this->typeHint)) {
+      $this->typeHint->replaceWith($type_hint);
+    }
+    else {
+      $this->prepend([
+        $type_hint,
+        new TokenNode(T_WHITESPACE, ' '),
+      ]);
+    }
+    return $this;
+  }
+
+  /**
    * @return TokenNode
    */
   public function getReference() {
@@ -74,10 +105,37 @@ class ParameterNode extends ParentNode {
   }
 
   /**
-   * @return Node
+   * @param boolean $is_reference
+   * @return $this
+   */
+  public function setReference($is_reference) {
+    if ($is_reference) {
+      if (!isset($this->reference)) {
+        $this->name->before(new TokenNode('&', '&'));
+      }
+    }
+    else {
+      if (isset($this->reference)) {
+        $this->reference->remove();
+      }
+    }
+    return $this;
+  }
+
+  /**
+   * @return TokenNode
    */
   public function getName() {
     return $this->name;
+  }
+
+  /**
+   * @param string $name
+   * @return $this
+   */
+  public function setName($name) {
+    $this->name->setText($name);
+    return $this;
   }
 
   /**
@@ -85,5 +143,32 @@ class ParameterNode extends ParentNode {
    */
   public function getValue() {
     return $this->value;
+  }
+
+  /**
+   * @param ExpressionNode|NULL $node
+   * @return $this
+   */
+  public function setValue($node) {
+    if ($node === NULL) {
+      if (isset($this->value)) {
+        $this->value->previousUntil(Filter::isInstanceOf('\Pharborist\VariableNode'))->remove();
+        $this->value->remove();
+      }
+    }
+    else {
+      if (isset($this->value)) {
+        $this->value->replaceWith($node);
+      }
+      else {
+        $this->append([
+          new TokenNode(T_WHITESPACE, ' '),
+          new TokenNode('=', '='),
+          new TokenNode(T_WHITESPACE, ' '),
+          $node,
+        ]);
+      }
+    }
+    return $this;
   }
 }

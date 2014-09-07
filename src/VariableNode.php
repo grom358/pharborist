2d964dcbd6b087ea856bdade1f6ca43c1e77fa09
Cameron Zemek <cameron.zemek@previousnext.com.au>
2014-09-07 21:58:35 +1000

    Added VariableNode::getName() and setName()

--- src/VariableNode.php
+++ src/VariableNode.php
@@ -7,5 +7,21 @@ namespace Pharborist;
  * For example, $a
  */
 class VariableNode extends TokenNode implements VariableExpressionNode, LexicalVariableNode {
+  /**
+   * @return string
+   *  The variable name, without the leading $.
+   */
+  public function getName() {
+    return ltrim($this->getText(), '$');
+  }
 
+  /**
+   * @param string $name
+   *  The name of the parameter, with or without the leading $.
+   *
+   * @return $this
+   */
+  public function setName($name) {
+    return $this->setText('$' . ltrim($name, '$'));
+  }
 }

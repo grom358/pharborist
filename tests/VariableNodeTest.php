2c6437e660a33160b2a5e3047db10334eca32880
phenaproxima <phenaproxima@205645.no-reply.drupal.org>
2014-09-07 21:14:12 -0400

    Wrote tests for VariableNode.

---
+++ tests/VariableNodeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pharborist;
+
+class VariableNodeTest extends \PHPUnit_Framework_TestCase {
+  public function testGetName() {
+    $var = new VariableNode(T_VARIABLE, '$form');
+    $this->assertEquals('form', $var->getName());
+  }
+
+  /**
+   * @depends testGetName
+   */
+  public function testSetName() {
+    $var = new VariableNode(T_VARIABLE, '$x');
+    $var->setName('$y');
+    $this->assertEquals('y', $var->getName());
+    $var->setName('z');
+    $this->assertEquals('z', $var->getName());
+  }
+}

3c09b70eef6c99b636309774c2e88ac0fec012c9
Cameron Zemek <cameron.zemek@previousnext.com.au>
2014-09-10 13:55:04 +1000

    Added ability to create line block comments

--- src/LineCommentBlockNode.php
+++ src/LineCommentBlockNode.php
@@ -6,6 +6,24 @@ namespace Pharborist;
  */
 class LineCommentBlockNode extends ParentNode {
   /**
+   * Create line comment block.
+   *
+   * @param string $comment
+   *   Comment without leading prefix.
+   * @return LineCommentBlockNode
+   */
+  public static function create($comment) {
+    $block_comment = new LineCommentBlockNode();
+    $comment = trim($comment);
+    $lines = array_map('trim', explode("\n", $comment));
+    foreach ($lines as $line) {
+      $comment_node = new CommentNode(T_COMMENT, '// ' . $line . "\n");
+      $block_comment->addChild($comment_node);
+    }
+    return $block_comment;
+  }
+
+  /**
    * @return string
    */
   public function getCommentText() {
@@ -25,5 +43,54 @@ class LineCommentBlockNode extends ParentNode {
       $child = $child->next;
     }
     return $comment;
+  }
+
+  /**
+   * Set indent for document comment.
+   *
+   * @param string $indent
+   *   Whitespace to use as indent.
+   * @return $this
+   */
+  public function setIndent($indent) {
+    // Normalize comment block.
+    $this->removeIndent();
+    // Add indent to comment block.
+    $this->addIndent($indent);
+    return $this;
+  }
+
+  /**
+   * Remove indent from document comment.
+   *
+   * @return $this
+   */
+  public function removeIndent() {
+    $this->children(function (Node $node) {
+      return !($node instanceof CommentNode);
+    })->remove();
+    return $this;
+  }
+
+  /**
+   * Add indent to comment.
+   *
+   * @param string $whitespace
+   *   Additional whitespace to add.
+   * @return $this
+   */
+  public function addIndent($whitespace) {
+    $has_indent = $this->children(function (Node $node) {
+      return !($node instanceof CommentNode);
+    })->count() > 0;
+    if ($has_indent) {
+      $this->children(Filter::isInstanceOf('\Pharborist\WhitespaceNode'))->each(function (WhitespaceNode $ws_node) use ($whitespace) {
+        $ws_node->setText($ws_node->getText() . $whitespace);
+      });
+    }
+    else {
+      $this->children()->before(Token::whitespace($whitespace));
+    }
+    return $this;
   }
 }

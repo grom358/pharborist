78c94b2745cced1f777189af5c234005407f600e
Phéna Proxima <djphenaproxima@gmail.com>
2014-09-09 22:38:36 -0400

    Added a getLineCount() test for a multi-line function call.

--- tests/StatementNodeTest.php
+++ tests/StatementNodeTest.php
@@ -16,5 +16,13 @@ END;
     $this->assertEquals(7, Parser::parseSnippet($text)->getLineCount());
     // What, you haven't seen Spaceballs?
     $this->assertEquals(1, Parser::parseSnippet('$combination = 12345;')->getLineCount());
+
+
+    $text = <<<'END'
+db_delete('variable')
+  ->condition('name', 'cron_last')
+  ->execute();
+END;
+    $this->assertEquals(3, Parser::parseSnippet($text)->getLineCount());
   }
 }

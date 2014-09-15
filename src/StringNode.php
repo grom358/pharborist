98b2d091a95c44dadcaa7b7096f3d3242a29c834
phenaproxima <phenaproxima@205645.no-reply.drupal.org>
2014-09-15 12:18:20 -0400

    Created StringNode::create().

--- src/StringNode.php
+++ src/StringNode.php
@@ -8,6 +8,18 @@ namespace Pharborist;
  */
 class StringNode extends TokenNode implements ExpressionNode {
   /**
+   * Creates a new constant string.
+   *
+   * @param string $text
+   *  The text of the string.
+   *
+   * @return \Pharborist\StringNode
+   */
+  public static function create($text) {
+    return new StringNode(T_CONSTANT_ENCAPSED_STRING, $text);
+  }
+
+  /**
    * Returns the original value of the string (unenclosed by quotes).
    *
    * @return string

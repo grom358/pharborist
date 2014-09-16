f288dc547568b1a0d391430131f2d63dedae5ac5
phenaproxima <phenaproxima@205645.no-reply.drupal.org>
2014-09-16 12:28:06 -0400

    Doc'd ClassMemberNode::create().

--- src/ClassMemberNode.php
+++ src/ClassMemberNode.php
@@ -15,6 +15,22 @@ class ClassMemberNode extends ParentNode {
    */
   protected $value;
 
+  /**
+   * Creates a new class member.
+   *
+   * @param string $name
+   *  The name of the member, with or without the leading $.
+   * @param \Pharborist\ExpressionNode $value
+   *  The default value of the member, if any.
+   * @param string $visibility
+   *  The member's visibility. Can be public, private, or protected. Defaults to
+   *  public.
+   *
+   * @return ClassMemberListNode
+   *
+   * @todo Not all expressions can be default values, but I forget what sorts of
+   * expressions are valid for this. Will need better sanity checking here.
+   */
   public static function create($name, ExpressionNode $value = NULL, $visibility = 'public') {
     $code = $visibility . ' $' . ltrim($name, '$');
     if ($value instanceof ExpressionNode) {

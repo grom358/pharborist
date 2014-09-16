2906752c782f73d96114d4d78df41ac6e7ce9477
phenaproxima <phenaproxima@205645.no-reply.drupal.org>
2014-09-16 09:50:15 -0400

    Documented FalseNode.

--- src/FalseNode.php
+++ src/FalseNode.php
@@ -2,16 +2,27 @@
 namespace Pharborist;
 
 /**
- * FALSE boolean.
+ * Boolean FALSE.
+ *
+ * <p>Represents the boolean FALSE constant, spelled either <code>FALSE</code>
+ * or <code>false</code>. This does <em>not</em> represent other falsy values like
+ * '' or 0.</p>
  */
 class FalseNode extends BooleanNode {
   /**
+   * Creates a new FalseNode.
+   *
    * @return FalseNode
    */
   public static function create($boolean = FALSE) {
     return BooleanNode::create(FALSE);
   }
 
+  /**
+   * Gets the boolean value of the node.
+   *
+   * @return boolean
+   */
   public function toBoolean() {
     return FALSE;
   }

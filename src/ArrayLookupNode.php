709ffae201d17630eb9629955f49b3da34dbf161
Phéna Proxima <djphenaproxima@gmail.com>
2014-10-10 13:10:41 -0400

    Created ArrayLookupNode::hasScalarKeys() and ArrayLookupNode::extractKeys().

--- src/ArrayLookupNode.php
+++ src/ArrayLookupNode.php
@@ -49,4 +49,38 @@ class ArrayLookupNode extends ParentNode implements VariableExpressionNode {
   public function getKey() {
     return $this->key;
   }
+
+  /**
+   * Returns TRUE if all keys in the lookup are scalar. So a lookup like
+   * $foo['bar']['baz'][0] will be TRUE, but $foo[$bar]['baz'][0] won't.
+   *
+   * @return boolean
+   */
+  public function hasScalarKeys() {
+    if ($this->key instanceof ScalarNode) {
+      return $this->array instanceof ArrayLookupNode ? $this->array->hasScalarKeys() : TRUE;
+    }
+    else {
+      return FALSE;
+    }
+  }
+
+  /**
+   * Returns every key in the lookup. For example, $foo['bar']['baz'][5] will
+   * return ['bar', 'baz', 5].
+   *
+   * @return mixed[]
+   *
+   * @throws \DomainException if the lookup contains any non-scalar keys.
+   */
+  public function extractKeys() {
+    if (!$this->hasScalarKeys()) {
+      throw new \DomainException('Cannot extract non-scalar keys from array lookup ' . $this);
+    }
+    $keys = [ $this->key->toValue() ];
+    if ($this->array instanceof ArrayLookupNode) {
+      $keys = array_merge($this->array->extractKeys(), $keys);
+    }
+    return $keys;
+  }
 }
--- tests/ArrayLookupNodeTest.php
+++ tests/ArrayLookupNodeTest.php
@@ -6,4 +6,20 @@ class ArrayLookupNodeTest extends \PHPUnit_Framework_TestCase {
     $lookup = ArrayLookupNode::create(Token::variable('$form_state'), new StringNode(T_CONSTANT_ENCAPSED_STRING, "'storage'"));
     $this->assertEquals('$form_state[\'storage\']', $lookup->getText());
   }
+
+  public function testHasScalarKeys() {
+    $this->assertTrue(Parser::parseExpression('$foo["bar"]["baz"][3]')->hasScalarKeys());
+    $this->assertFalse(Parser::parseExpression('$foo[$bar]["baz"]')->hasScalarKeys());
+  }
+
+  public function testExtractKeys() {
+    $this->assertSame(['bar', 'baz', 3], Parser::parseExpression('$foo["bar"]["baz"][3]')->extractKeys());
+  }
+
+  /**
+   * @expectedException \DomainException
+   */
+  public function testExtractNonScalarKeys() {
+    Parser::parseExpression('$foo[$bar][baz()][30]')->extractKeys();
+  }
 }

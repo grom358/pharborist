34c68def047613576e5e3a42679561b11c73c7db
phenaproxima <phenaproxima@205645.no-reply.drupal.org>
2014-10-22 16:24:20 -0400

    Wrote a test for getKeys().

--- tests/ArrayLookupNodeTest.php
+++ tests/ArrayLookupNodeTest.php
@@ -7,6 +7,17 @@ class ArrayLookupNodeTest extends \PHPUnit_Framework_TestCase {
     $this->assertEquals('$form_state[\'storage\']', $lookup->getText());
   }
 
+  public function testGetKeys() {
+    $lookup = Parser::parseExpression('$foo["bar"]["baz"]');
+    $keys = $lookup->getKeys();
+    $this->assertInternalType('array', $keys);
+    $this->assertCount(2, $keys);
+    $this->assertInstanceOf('\Pharborist\StringNode', $keys[0]);
+    $this->assertEquals('bar', $keys[0]->toValue());
+    $this->assertInstanceOf('\Pharborist\StringNode', $keys[1]);
+    $this->assertEquals('baz', $keys[1]->toValue());
+  }
+
   public function testHasScalarKeys() {
     $this->assertTrue(Parser::parseExpression('$foo["bar"]["baz"][3]')->hasScalarKeys());
     $this->assertFalse(Parser::parseExpression('$foo[$bar]["baz"]')->hasScalarKeys());

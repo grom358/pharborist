7511a2c5edd2375255da35b33ec61c601fa3fe37
Cameron Zemek <cameron.zemek@previousnext.com.au>
2014-10-26 00:17:46 +1000

    Updated doc comments

--- src/ObjectPropertyNode.php
+++ src/ObjectPropertyNode.php
@@ -30,9 +30,11 @@ class ObjectPropertyNode extends ParentNode implements VariableExpressionNode {
   }
 
   /**
-   * Returns the name of the property if it's a T_STRING.
+   * Returns the name of the property if it's an identifier (ie. T_STRING TokenNode).
    *
    * @return string|NULL
+   *   Name of the property or NULL if not an identifier (eg. dynamic property
+   *   name).
    */
   public function getPropertyName() {
     $root_property = $this->getRootProperty();
@@ -42,12 +44,14 @@ class ObjectPropertyNode extends ParentNode implements VariableExpressionNode {
   }
 
   /**
-   * Returns the root of the property.
+   * Returns the root property.
    *
    * For example, given an expression like $node->body['und'][0]['value'],
-   * this method will return a T_STRING TokenNode for 'body'.
+   * this method will return the identifier (T_STRING TokenNode) 'body'. Or,
+   * given an expression like $foo->bar->baz, will return the identifier 'bar'.
    *
    * @return Node
+   *   A node for the root property.
    */
   public function getRootProperty() {
     if ($this->property instanceof ObjectPropertyNode) {

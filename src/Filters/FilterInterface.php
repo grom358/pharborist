de05ce6c8598e8eae7719d335334c69141b817f5
phenaproxima <phenaproxima@205645.no-reply.drupal.org>
2014-11-03 10:02:24 -0500

    Documented FilterInterface.

--- src/Filters/FilterInterface.php
+++ src/Filters/FilterInterface.php
@@ -9,26 +9,87 @@ use Pharborist\Node;
  */
 interface FilterInterface {
 
+  /**
+   * Tests a single node against this filter.
+   *
+   * @param Node $node
+   *
+   * @return boolean
+   */
   public function __invoke(Node $node);
 
+  /**
+   * Returns if the origin matches the filter.
+   *
+   * @return boolean
+   */
   public function isMatch();
 
+  /**
+   * Returns if the origin has a descendant which matches the filter.
+   *
+   * @return boolean
+   */
   public function hasMatch();
 
+  /**
+   * Returns children of the origin which match the filter.
+   *
+   * @return \Pharborist\NodeCollection
+   */
   public function children();
 
+  /**
+   * Returns all descendants of the origin which match the filter.
+   *
+   * @return \Pharborist\NodeCollection
+   */
   public function find();
 
+  /**
+   * Returns if the origin's parent matches the filter.
+   *
+   * @return boolean
+   */
   public function parentIsMatch();
 
+  /**
+   * Returns parents of the origin which match the filter.
+   *
+   * @return \Pharborist\NodeCollection
+   */
   public function parents();
 
+  /**
+   * Returns siblings of the origin which match the filter.
+   *
+   * @return \Pharborist\NodeCollection
+   */
   public function siblings();
 
+  /**
+   * Returns if the origin's previous sibling matches the filter.
+   *
+   * @return boolean
+   */
   public function previousIsMatch();
 
+  /**
+   * Returns the origin's previous siblings, filtered.
+   *
+   * @return \Pharborist\NodeCollection
+   */
   public function previousAll();
 
+  /**
+   * Returns the origin's previous siblings, filtered, stopping at the first
+   * one which matches the predicate.
+   *
+   * @param callable $until
+   * @param bool $inclusive
+   *
+   * @return \Pharborist\NodeCollection
+   */
   public function previousUntil(callable $until, $inclusive = TRUE);
 
   public function nextIsMatch();

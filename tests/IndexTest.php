f6c348d2f874bb48f6beda8afaa1bc85ca1b5d98
Phéna Proxima <djphenaproxima@gmail.com>
2015-04-21 22:26:16 -0400

    Began writing (non-passing, fatal-causing) tests for the index.

--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -5,6 +5,8 @@ use Pharborist\Index\Indexer;
 use Pharborist\Index\ProjectIndex;
 
 class IndexTest extends \PHPUnit_Framework_TestCase {
+  protected $index;
+
   public function testIndex() {
     chdir(dirname(__FILE__));
     $indexer = new Indexer();
@@ -36,5 +38,305 @@ class IndexTest extends \PHPUnit_Framework_TestCase {
     $index = $indexer->index();
     $this->assertEmpty($index->getFiles());
     $this->assertEmpty($index->getClasses());
+  }
+
+  public function setUp() {
+    // @TODO Build $this->index
+    // Knights\Lancelot is a class
+    // Knight is an interface
+  }
+
+  /**
+   * ProjectIndex::getFiles() should return a collection of FileIndex, keyed by
+   * file name.
+   */
+  public function testGetFiles() {
+    $files = $this->index->getFiles();
+    $this->assertInstanceOf('\Doctrine\Common\Collections\Collection', $files);
+
+    $keys = [
+      'Knight.php',
+      'Camelot.php',
+      'Lancelot.php',
+      'Galahad.php',
+      'Robin.php',
+      'Tim.php',
+      'BraveTrait.php',
+      'PureTrait.php',
+      'CowardTrait.php',
+    ];
+    $this->assertEquals($keys, $files->getKeys());
+
+    foreach ($keys as $key) {
+      $this->assertInstanceOf('\Pharborist\Index\FileIndex', $files[$key]);
+    }
+    $this->assertInstanceOf('\Pharborist\Index\FileIndex', $this->index->getFile('Knight.php'));
+  }
+
+  /**
+   * ProjectIndex::getClasses() should return a collection of ClassIndex,
+   * keyed by fully-qualified name.
+   */
+  public function testGetClasses() {
+    $classes = $this->index->getClasses();
+    $this->assertInstanceOf('\Doctrine\Common\Collections\Collection', $classes);
+    $this->assertTrue($classes->containsKey('Knights\Lancelot'));
+    $this->assertInstanceOf('\Pharborist\Index\ClassIndex', $classes['Knights\Lancelot']);
+    $this->assertInstanceOf('\Pharborist\Index\ClassIndex', $this->index->getClass('Knights\Lancelot'));
+  }
+
+  /**
+   * ProjectIndex::getInterfaces() should return a collection of InterfaceIndex,
+   * keyed by fully-qualified name.
+   */
+  public function testGetInterfaces() {
+    $interfaces = $this->index->getClasses();
+    $this->assertInstanceOf('\Doctrine\Common\Collections\Collection', $interfaces);
+    $this->assertTrue($interfaces->containsKey('Knight'));
+    $this->assertInstanceOf('\Pharborist\Index\InterfaceIndex', $interfaces['Knight']);
+    $this->assertInstanceOf('\Pharborist\Index\InterfaceIndex', $this->index->getInterface('Knight'));
+  }
+
+  /**
+   * ProjectIndex::getTraits() should return a collection of TraitIndex,
+   * keyed by fully-qualified name.
+   */
+  public function testGetTraits() {
+    $traits = $this->index->getTraits();
+    $this->assertInstanceOf('\Doctrine\Common\Collections\Collection', $traits);
+
+    $keys = [
+      'BraveTrait',
+      'PureTrait',
+      'CowardTrait',
+    ];
+    $this->assertEquals($keys, $traits->getKeys());
+
+    foreach ($keys as $key) {
+      $this->assertInstanceOf('\Pharborist\Index\TraitIndex', $traits[$key]);
+    }
+    $this->assertInstanceOf('\Pharborist\Index\TraitIndex', $this->index->getTrait('BraveTrait'));
+  }
+
+  /**
+   * ProjectIndex::getFunctions() should return a collection of FunctionIndex
+   * keyed by name.
+   */
+  public function testGetFunctions() {
+    $functions = $this->index->getFunctions();
+    $this->assertInstanceOf('\Doctrine\Common\Collections\Collection', $functions);
+
+    $keys = [
+      'create_fireball',
+      'be_menacing',
+      'be_scottish',
+      'set_rabbit_warning',
+    ];
+    $this->assertEquals($keys, $functions->getKeys());
+
+    foreach ($keys as $key) {
+      $this->assertInstanceOf('\Pharborist\Index\FunctionIndex', $functions[$key]);
+    }
+    $this->assertInstanceOf('\Pharborist\Index\TraitIndex', $this->index->getFunction('create_fireball'));
+  }
+
+  /**
+   * ProjectIndex::getConstants() should return a collection of ConstantIndex
+   * keyed by fully-qualified name.
+   */
+  public function testGetConstants() {
+    $constants = $this->index->getConstants();
+    $this->assertInstanceOf('\Doctrine\Common\Collections\Collection', $constants);
+
+    $keys = [
+      'TIM_ACCENT',
+      'Knights\Lancelot::GENDER',
+      'Knights\Galahad::GENDER',
+      'Knights\Robin::GENDER',
+    ];
+    $this->assertEquals($keys, $constants->getKeys());
+
+    foreach ($keys as $key) {
+      $this->assertInstanceOf('\Pharborist\Index\ConstantIndex', $constants[$key]);
+    }
+    $this->assertInstanceOf('\Pharborist\Index\ConstantIndex', $this->index->getConstant('TIM_ACCENT'));
+  }
+
+  /**
+   * ClassIndex::getProperties() should return a collection of PropertyIndex,
+   * keyed by name.
+   */
+  public function testGetClassProperties() {
+    $class = $this->index->getClass('Knights\Lancelot');
+    $properties = $class->getProperties();
+    $this->assertInstanceOf('\Doctrine\Common\Collections\Collection', $properties);
+
+    $keys = [
+      'sword',
+      'isBerserk',
+    ];
+    $this->assertEquals($keys, $properties->getKeys());
+
+    foreach ($keys as $key) {
+      $this->assertInstanceOf('\Pharborist\Index\PropertyIndex', $properties[$key]);
+    }
+    $this->assertInstanceOf('\Pharborist\Index\PropertyIndex', $properties->get('sword'));
+  }
+
+  public function testGetClassOwnProperties() {
+    // $index->getClass('foo')->getOwnProperties() returns a collection of
+    // PropertyIndex keyed by name
+  }
+
+  public function testGetClassMethods() {
+    // $index->getClass('foo')->getMethods() returns a collection of
+    // MethodIndex keyed by name
+  }
+
+  public function testGetClassOwnMethods() {
+    // $index->getClass('foo')->getOwnMethods() returns a collection of
+    // MethodIndex keyed by name
+  }
+
+  public function testGetClassConstants() {
+    // $index->getClass('foo')->getConstants() returns a collection of
+    // ConstantIndex keyed by name
+  }
+
+  public function testGetClassOwnConstants() {
+    // $index->getClass('foo')->getOwnConstants() returns a collection of
+    // ConstantIndex keyed by name
+  }
+
+  public function testGetInterfaceMethods() {
+    // $index->getInterface('foo')->getMethods() returns a collection of
+    // MethodIndex keyed by name
+  }
+
+  public function testGetInterfaceOwnMethods() {
+    // $index->getInterface('foo')->getOwnMethods() returns a collection of
+    // MethodIndex keyed by name
+  }
+
+  public function testGetInterfaceConstants() {
+    // $index->getInterface('foo')->getConstants() returns a collection of
+    // ConstantIndex keyed by name
+  }
+
+  public function testGetInterfaceOwnConstants() {
+    // $index->getInterface('foo')->getOwnConstants() returns a collection of
+    // ConstantIndex keyed by name
+  }
+
+  public function testGetTraitProperties() {
+    // $index->getTrait('foo')->getProperties() returns a collection of
+    // PropertyIndex keyed by name
+  }
+
+  public function testGetTraitOwnProperties() {
+    // $index->getTrait('foo')->getOwnProperties() returns a collection of
+    // PropertyIndex keyed by name
+  }
+
+  public function testGetTraitMethods() {
+    // $index->getTrait('foo')->getMethods() returns a collection of
+    // MethodIndex keyed by name
+  }
+
+  public function testGetTraitOwnMethods() {
+    // $index->getTrait('foo')->getOwnMethods() returns a collection of
+    // MethodIndex keyed by name
+  }
+
+  public function testGetTraitConstants() {
+    // $index->getTrait('foo')->getConstants() returns a collection of
+    // ConstantIndex keyed by name
+  }
+
+  public function testGetTraitOwnConstants() {
+    // $index->getTrait('foo')->getOwnConstants() returns a collection of
+    // ConstantIndex keyed by name
+  }
+
+  public function testGetClassParent() {
+    // $index->getClass('foo')->getParent() returns a ClassIndex or NULL
+  }
+
+  public function testGetClassInterfaces() {
+    // $index->getClass('foo')->getInterfaces() returns a collection of
+    // InterfaceIndex keyed by fully qualified name
+  }
+
+  public function testGetClassTraits() {
+    // $index->getClass('foo')->getTraits() returns a collection of TraitIndex
+    // keyed by fully qualified name
+  }
+
+  public function testGetClassExtendedBy() {
+    // $index->getClass('foo')->extendedBy() returns a collection of
+    // ClassIndex keyed by fully qualified name
+  }
+
+  public function testGetInterfaceParents() {
+    // $index->getInterface('foo')->getParents() returns a collection of
+    // InterfaceIndex keyed by fully qualified name
+  }
+
+  public function testGetInterfaceExtendedBy() {
+    // $index->getInterface('foo')->extendedBy() returns a collection of
+    // InterfaceIndex keyed by fully qualified name
+  }
+
+  public function testGetInterfaceImplementedBy() {
+    // $index->getInterface('foo')->implementedBy() returns a collection of
+    // ClassIndex keyed by fully qualified name
+  }
+
+  public function testGetTraitClasses() {
+    // $index->getTrait('foo')->getClasses() returns a collection of
+    // ClassIndex keyed by fully qualified name
+  }
+
+  public function testGetTraitTraitsUsing() {
+    // $index->getTrait('foo')->getTraitsUsing() returns a collection of
+    // TraitIndex keyed by fully qualified name
+  }
+
+  public function testGetTraitTraitsUsed() {
+    // $index->getTrait('foo')->getTraitsUsed() returns a collection of
+    // TraitIndex keyed by fully qualified name
+  }
+
+  public function testGetClassInstances() {
+    // $index->getClass('foo')->getInstances() returns SourcePosition[]
+  }
+
+  public function testGetFunctionCalls() {
+    // $index->getFunction('foo')->getCalls() returns SourcePosition[]
+  }
+
+  public function testFileAdd() {
+    // $index->addFile('foo') should add classes, functions, constants,
+    // interfaces, and traits it contains
+  }
+
+  public function testFileUpdate() {
+    // $index->getFile('foo')->update() should re-index the file (call
+    // delete(), then add())
+  }
+
+  public function testFileDelete() {
+    // $index->getFile('foo')->delete() should delete classes, functions,
+    // constants, interfaces, and traits it contains
+  }
+
+  public function testMethodAnnotation() {
+    // $index->getClass('foo')->getMethod('baz') should return a MethodIndex
+    // for @method mixed baz(integer $bar)
+  }
+
+  public function testPropertyAnnotation() {
+    // $index->getClass('foo')->getProperty('baz') should return a
+    // PropertyIndex for @property string $baz
   }
 }

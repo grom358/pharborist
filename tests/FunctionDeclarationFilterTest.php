8fa401a365020110b7b77126d16a3f8ff7dbe02c
Phéna Proxima <djphenaproxima@gmail.com>
2015-04-27 12:12:31 -0400

    Made test suite pass.

--- tests/FunctionDeclarationFilterTest.php
+++ tests/FunctionDeclarationFilterTest.php
@@ -6,13 +6,13 @@ use Pharborist\Filters\FunctionDeclarationFilter;
 
 class FunctionDeclarationFilterTest extends \PHPUnit_Framework_TestCase {
   public function testPassFunction() {
-    $filter = new FunctionDeclarationFilter();
+    $filter = new FunctionDeclarationFilter(['foobaz']);
     $func = Parser::parseSnippet('function foobaz() {}');
     $this->assertTrue($filter($func));
   }
 
   public function testFailNonFunction() {
-    $filter = new FunctionDeclarationFilter();
+    $filter = new FunctionDeclarationFilter(['foobaz']);
     $var = Token::variable('$baz');
     $this->assertFalse($filter($var));
   }
